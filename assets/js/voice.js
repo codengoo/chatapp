a7bd90a57154f28af70b6f3257c092bcb8b819a3
NghiaDT <nghiadt22.work@gmail.com>
2024-06-27 15:20:06 +0700

    update timer

--- assets/js/voice.js
+++ assets/js/voice.js
@@ -7,6 +7,7 @@ const btnDelete = document.getElementById("btn_delete");
 const btnCall = document.getElementById("btn_call");
 const btnSend = document.getElementById("btn_send");
 const btnSendAudio = document.getElementById("btn_send_audio");
+const countTime = document.getElementById("count_time_record");
 
 const inp_msg = document.getElementById("inp_msg");
 const audio_preview = document.getElementById("audio_preview");
@@ -15,6 +16,7 @@ audio_preview.style.display = "none";
 btnStop.style.display = "none";
 btnDelete.style.display = "none";
 btnSendAudio.style.display = "none";
+countTime.style.display = "none";
 
 var AudioContext = window.AudioContext || window.webkitAudioContext;
 var audioContext;
@@ -23,16 +25,43 @@ var blob;
 var recordControl;
 var input;
 var gumStream;
+var count = 0;
+var timer;
 
 const { chat, userID } = getConstants();
+
+function timeCounter() {
+    count++;
+    countTime.innerText = secondsToHHMMSS(count);
+}
+
+function secondsToHHMMSS(seconds) {
+    const hours = Math.floor(seconds / 3600);
+    const minutes = Math.floor((seconds % 3600) / 60);
+    const secondsLeft = seconds % 60;
+
+    const formattedHours = hours.toString().padStart(2, '0');
+    const formattedMinutes = minutes.toString().padStart(2, '0');
+    const formattedSeconds = secondsLeft.toString().padStart(2, '0');
+
+    return `${formattedHours}:${formattedMinutes}:${formattedSeconds}`;
+}
 
 btnRecord.addEventListener("click", function () {
     startRecording();
     this.style.display = "none";
     btnSend.style.display = "none";
+    inp_msg.style.display = "none";
+    audio_preview.style.display = "none";
+    btnDelete.style.display = "none";
+    btnCall.style.display = "none";
 
     btnStop.style.display = "block";
     btnSendAudio.style.display = "block";
+    countTime.style.display = "block";
+    count = 0;
+    timer = setInterval(timeCounter, 1000)
+    countTime.innerText = "00:00:00";
 });
 
 btnStop.addEventListener("click", function () {
@@ -40,10 +69,13 @@ btnStop.addEventListener("click", function () {
     inp_msg.style.display = "none";
     btnCall.style.display = "none";
     this.style.display = "none";
+    countTime.style.display = "none";
 
     audio_preview.style.display = "block";
     btnRecord.style.display = "block";
     btnDelete.style.display = "block";
+    count = 0;
+    clearInterval(timer);
 });
 
 btnDelete.addEventListener("click", function () {
--- components/input.php
+++ components/input.php
@@ -2,13 +2,8 @@
     <div class="d-flex gap-3">
         <input type="text" class="form-control" id="inp_msg" placeholder="Viết tin nhắn ở đây">
         <audio controls id="audio_preview" class="flex-grow-1"></audio>
+        <div id="count_time_record" class="bg-primary text-white p-2 fw-bold flex-grow-1 text-start">00:00:00</div>
 
-        <button id="btn_send" class="btn btn-primary" type="button">
-            <img src="./assets/images/send.svg" />
-        </button>
-        <button id="btn_send_audio" class="btn btn-info" type="button">
-            <img src="./assets/images/send.svg" />
-        </button>
         <button id="btn_expand" class="btn btn-warning" type="button">
             <img src="./assets/images/expand.svg" />
         </button>
@@ -23,6 +18,13 @@
         </button>
         <button id="btn_record" class="btn btn-info" type="button" title="Tin nhắn thoại">
             <img src="./assets/images/mic.svg" />
+        </button>
+
+        <button id="btn_send" class="btn btn-primary" type="button">
+            <img src="./assets/images/send.svg" />
+        </button>
+        <button id="btn_send_audio" class="btn btn-primary" type="button">
+            <img src="./assets/images/send.svg" />
         </button>
     </div>
 </form>
